9a45cd815c5af86021d7fa17cd4f51cf3e1dec46
Brian_Sablich <sablichbrian@yahoo.com.ar>
2024-02-12 23:29:34 -0300

    contruccion presentacion nueva
    primer commit de la nueva presentacion

--- funcionesPortfolio.js
+++ funcionesPortfolio.js
@@ -45,22 +45,35 @@
 
         //JS DE ARCHIVOS DE PRESENTACION
 
-        document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('DOMContentLoaded', function() {
             const archivos = document.querySelectorAll('.archivo');
-
+            let clickCount = 0;
+            let timer;
+        
             archivos.forEach(archivo => {
-                archivo.addEventListener('click', function (e) {
+                archivo.addEventListener('click', function(e) {
                     e.preventDefault();
-                    const texto = this.getAttribute('data-texto');
-                    mostrarTexto(texto);
+                    clickCount++;
+                    if (clickCount === 1) {
+                        timer = setTimeout(function() {
+                            clickCount = 0;
+                        }, 300);
+                    } else if (clickCount === 2) {
+                        const texto = archivo.getAttribute('data-texto');
+                        mostrarTexto(texto);
+                        clearTimeout(timer);
+                        clickCount = 0;
+                        // Aquí puedes agregar una acción adicional para el doble clic si lo deseas
+                    }
                 });
             });
-
+        
             function mostrarTexto(texto) {
                 const contenedorTexto = document.getElementById('textoArchivo');
                 contenedorTexto.textContent = texto;
             }
         });
+        
 
         //FIN JS DE ARCHIVOS DE PRESENTACION
 
